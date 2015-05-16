e1ec435fb767760aedd382aace96ac41ccc0e78f
Neeraj Singh <neeraj@bigbinary.com>
2015-05-15 20:41:13 -0400

    wip

--- todo-app/1-basic-reactjs-app/app.js
+++ todo-app/1-basic-reactjs-app/app.js
@@ -2,16 +2,12 @@ var React = require('react');
 
 var HelloMessage = React.createClass({
 
-
   render () {
-    greeting = 'Hello';
-    msg = `${greeting} ${this.props.name}`
-    console.log(msg);
-
-    return  (
-        <div> { msg } </div>
+    return (
+      <div> Hello John </div>
     );
   }
+
 });
 
-React.render(<HelloMessage name="John7" />, document.body);
+React.render(<HelloMessage />, document.body);
